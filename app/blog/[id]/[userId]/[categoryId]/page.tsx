837a34aaf3d6e0261a71f84651febedeb89567a2
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-05-18 17:57:54 +0900

    catch-all-segmentsの設定

--- app/blog/[id]/[userId]/[categoryId]/page.tsx
+++ app/blog/[id]/[userId]/[categoryId]/page.tsx
@@ -1,4 +1,4 @@
-const Page = ({ params }: { params: { id: string[] } }) => {
+const Page = ({ params }: { params: { id: string; userId: string; categoryId: string } }) => {
   console.log(params)
   return <div className="m-4 font-bold">Blog ID: </div>
 }
