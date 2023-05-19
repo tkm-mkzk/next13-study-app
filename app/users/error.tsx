ab4602cb6b8a5741e9acf62c7aa6dfe6c4e96e27
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-05-19 16:14:38 +0900

    エラー画面からの復帰

--- app/users/error.tsx
+++ app/users/error.tsx
@@ -1,9 +1,12 @@
 'use client'
 
-export default function Error({ error }: { error: Error }) {
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   return (
     <div className="m-4 font-bold">
       <p>{error.message}</p>
+      <button className="rounded-lg bg-blue-500 px-2 py-1 text-white" onClick={() => reset()}>
+        Try again
+      </button>
     </div>
   )
 }
