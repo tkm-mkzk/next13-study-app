91ad08b8de68821b3f43ebf3c7e75f3c664137f9
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-05-19 15:24:52 +0900

    手動でのSuspenseの設定

--- app/users/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-  return <div className="flex h-screen items-center justify-center font-bold">ローディング中</div>
-}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,19 @@
+import { Suspense } from 'react'
 import UserList from './UserList'
+import OtherUserList from './OtherUserList'
 
 const Page = async () => {
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">ユーザー一覧</h1>
-      {/* @ts-expect-error Async Server Component */}
-      <UserList />
+      <Suspense fallback={<p className="text-red-700">Loading UserList...</p>}>
+        {/* @ts-expect-error Async Server Component */}
+        <UserList />
+      </Suspense>
+      <Suspense fallback={<p>Loading OtherUserList...</p>}>
+        {/* @ts-expect-error Async Server Component */}
+        <OtherUserList />
+      </Suspense>
     </div>
   )
 }
